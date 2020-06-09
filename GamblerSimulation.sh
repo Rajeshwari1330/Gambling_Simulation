071fa8662b88bac0292fa0ad395ce4cfaa6ba960
misti <yallalraj1307@gmail.com>
2020-06-09 21:56:22 +0530

    ADDED: Luckiest and Unluckiest day for gambler

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -x
 echo "Welcome to Gambling Simulation Problem ...!!!"
 
+#!/bin/bash -x
+
 function flip()
 {
 	result=$((RANDOM%2 + 1))
@@ -10,114 +12,119 @@ function flip()
         add=1;
 	elif(($result==2))
 	then
-    	echo "OUTPUT IS TAIL"
+        echo "OUTPUT IS TAIL"
         sub=1;
 	fi
 }
 
-echo "Enter the number of months you want to play"
+echo "enter the number of months you want to play"
 read monthNumber
 month=0
-for((month=0; month<monthNumber; month++))
+for((month=0;month<monthNumber;month++))
 do
-echo "Do you want to continue same input for 20 days or want to change it"
-echo "For same press 1"
-echo "Different input for each day press 2"
-read input
+	echo "do you want to continue same input for 20 days or want to change it"
+	echo "For same press 1"
+	echo "Different input for each day press 2"
+	read input
 
-if(($input==1))
-then
-	echo "Which do you want to choose for $month_number months:"
-    echo "---------------"
-    echo "1.HEAD"
-    echo "2.TAIL"
-    read user
+	if(($input==1))
+	then
+    	echo "Which do you want to choose for $monthNumber months:"
+        echo "---------------"
+        echo "1.HEAD"
+        echo "2.TAIL"
+        read user
 
-	echo "Till how much percentage of the amount you want to play?"
-    read stakeAmount
+		echo "Till how much percentage of the amount you want to play?"
+        read stakeAmount
 
-	totalDays=21
-	for((day=1; day<totalDays; day++))
-	do
-		echo "----------DAY $day -------------"
+		totalDays=21
+		for((day=1;day<totalDays;day++))
+		do
+			echo "----------DAY $day -------------"
 
-		if(($user == 1 || $user ==2))
-		then
-        	headIncrement=1
-        	tailIncrement=1
-        	money=100
-        	echo "I have $ 100 with me"
-        	bet=0
-        	echo "I will bet $ 1 now"
-
-			while(($money != 200 || $money != 0))
-        	do
-            	flip
-                if(($result==1))
-                then
-                	echo "won $headIncrement time"
-                    money=`expr $money + $add`
-                  	echo "money is $ $money"
-                    headIncrement=`expr $headIncrement + 1`
-                elif(($result==2))
-                then
-                	echo "won $tailIncrement time"
-                    money=`expr $money - $sub`
-                   	echo "money is $ $money"
-                    tailIncrement=`expr $tailIncrement + 1`
-               	fi
-
-		        winAmount=`expr $stakeAmount + 100`
-               	lossAmount=`expr 100 - $stakeAmount`
-                bet=`expr $bet + 1`
-                echo "number of bet is $bet"
-
-                if(($money == $winAmount))
-                then
-                	echo "won $ $winAmount for $ $stakeAmount %"
-                    break
-               	elif(($money == $lossAmount))
-                then
-                	echo "lost $ $lossAmount for $ $stakeAmount %"
-                    break
-                fi
-        	done
-			echo "--------------------------------"
-       		echo "money for day $day is $ $money"
-        	totalMoneyWon+=( "$money" )
-			echo "--------------------------------"
-
-			if(($money>100))
+			if(($user == 1 || $user ==2))
 			then
-				dayWon+=( "$day" )
-			elif(($money<100))
-			then
-				dayLoss+=( "$day")
-			fi
-			totalMoneyWon+=( "$money" )
+        		headIncrement=1
+        		tailIncrement=1
+        		money=100
+        		echo "I have $ 100 with me"
+        		bet=0
+        		echo "I will bet $ 1 now"
 
-			echo "----------------------------"
-			for i in ${dayWon[@]}
-			do
-            	echo "winning day is $i"
-        	done
+				while(($money != 200 || $money != 0))
+        		do
+                	flip
+                	if(($result==1))
+                	then
+                    	echo "won $headIncrement time"
+                        money=`expr $money + $add`
+                        echo "money is $ $money"
+                        headIncrement=`expr $headIncrement + 1`
+                	elif(($result==2))
+                	then
+                    	echo "won $tailIncrement time"
+                        money=`expr $money - $sub`
+                        echo "money is $ $money"
+                        tailIncrement=`expr $tailIncrement + 1`
+                	fi
 
-		    for i in ${dayLoss[@]}
-        	do
-        		echo "lossing day is $i"
-        	done
-			echo "----------------------------"
+		            winAmount=`expr $stakeAmount + 100`
+                	lossAmount=`expr 100 - $stakeAmount`
+                	bet=`expr $bet + 1`
+                	echo "number of bet is $bet"
 
-		else
-        	echo "enter the correct input out of two"
-        	exit
+                	if(($money == $winAmount))
+                	then
+                    	echo "won $ $winAmount for $ $stakeAmount %"
+                        break
+                	elif(($money == $lossAmount))
+                	then
+                    	echo "lost $ $lossAmount for $ $stakeAmount %"
+                        break
+                	fi
+        		done
+				echo "--------------------------------"
+       			echo "money for day $day is $ $money"
+        		totalMoneyWon+=( "$money" )
+				echo "--------------------------------"
+
+				if(($money>100))
+				then
+					dayWon+=( "$day" )
+				elif(($money<100))
+				then
+					dayLoss+=( "$day")
+				fi
+				totalMoneyWon+=( "$money" )
+
+				echo "----------------------------"
+				for i in ${dayWon[@]}
+				do
+					echo "^^^^^^^^^^^^^^^^^^^^^^"
+                	echo "winning day is $i"
+					echo "^^^^^^^^^^^^^^^^^^"
+        			echo "my luckiest day is $i"
+				done
+
+		        for i in ${dayLoss[@]}
+        		do
+					echo "^^^^^^^^^^^^^^^^^^"
+        		    echo "lossing day is $i"
+					echo "^^^^^^^^^^^^^^^^^^"
+					echo "my unclucky day is $i"
+        		done
+				echo "----------------------------"
+			else
+        		echo "enter the correct input out of two"
+        		exit
 		fi
 	done
 
 	elif(($input==2))
 	then
 		totalDays=21
-		for((day=1;day<totalDays;day++))
+		for((day=1; day<totalDays; day++))
 		do
 			echo "which do you want to choose $month_number month:"
         	echo "---------------"
@@ -137,24 +144,25 @@ then
         		echo "I have $ 100 with me"
         		bet=0
         		echo "I will bet $ 1 now"
+
         		while(($money != 200 || $money != 0))
         		do
                 	flip
                 	if(($result==1))
                 	then
                     	echo "won $headIncrement time"
-                       	money=`expr $money + $add`
+                        money=`expr $money + $add`
                         echo "money is $ $money"
-                       	headIncrement=`expr $headIncrement + 1`
+                        headIncrement=`expr $headIncrement + 1`
                 	elif(($result==2))
                 	then
                     	echo "won $tailIncrement time"
-                        money=`expr $money - $sub`
+                       	money=`expr $money - $sub`
                         echo "money is $ $money"
                         tailIncrement=`expr $tailIncrement + 1`
                 	fi
 
-		           	winAmount=`expr $stakeAmount + 100`
+		            winAmount=`expr $stakeAmount + 100`
                 	lossAmount=`expr 100 - $stakeAmount`
                 	bet=`expr $bet + 1`
                 	echo "number of bet is $bet"
@@ -162,13 +170,14 @@ then
                 	if(($money == $winAmount))
                 	then
                     	echo "won $ $winAmount for $ $stakeAmount %"
-                        break
+                       	break
                 	elif(($money == $lossAmount))
                 	then
-                        echo "lost $ $lossAmount for $ $stakeAmount %"
+                    	echo "lost $ $lossAmount for $ $stakeAmount %"
                         break
                 	fi
         		done
+				echo "--------------------------------"
 				echo "--------------------------------"
        			echo "money for day $day is $ $money"
 				echo "--------------------------------"
@@ -184,6 +193,8 @@ then
 				totalMoneyWon+=( "$money" )
 
 				echo "----------------------------"
+				echo "----------------------------"
+
 				for i in ${dayWon[@]}
 				do
                 	echo "winning day is $i"
@@ -194,11 +205,12 @@ then
         			echo "lossing day is $i"
         		done
 				echo "----------------------------"
+				echo "----------------------------"
 			else
         		echo "enter the correct input out of two"
         		exit
 			fi
-	done
+		done
 	else
 		echo "enter the correct input out of 2"
 	fi
@@ -213,9 +225,8 @@ then
 	echo "-----------------------------------------------"
 	echo "Total money won after playing 20 days is $ $sum for $monthNumber months"
 	echo "---------------------------------------------"
-	echo "you bet $ $betDollars"
-
-	if(($sum < $betDollars))
+	echo "You bet $betDollars"
+	if(( $sum < $betDollars))
 	then
 		moneyLoss=`expr $betDollars - $sum `
 		echo "Total money loss in $betDays days is $ $moneyLoss"
@@ -224,8 +235,3 @@ then
 		exit
 	fi
 done
-
-
-
-
-
