aee20b4f49bcf9d5b7bb5b25af4340c63d1a2a5a
misti <yallalraj1307@gmail.com>
2020-06-09 20:57:14 +0530

    ADDED: Declaration of stake and bet variable

--- GamblerSimulation.sh
+++ GamblerSimulation.sh
@@ -1,1 +1,14 @@
+#!/boin/bash -x
 echo "Welcome to Gambling Simulation Problem ...!!!"
+
+echo "How many times you want to play?"
+read numberOfGames
+
+for((i=0; i<numberOfGames; i++))
+do
+    money=100
+    echo "I have $ 100 with me"
+    bet=0
+    echo "I will bet $ 1 now"
+done
+
